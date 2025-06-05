179e0d9bd67a46f4815a5b16aef21347ff122fe3
Svejest1 <126154421+Svejest1@users.noreply.github.com>
2025-06-05 04:39:11 +0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,28 +1,14 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
-const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
+const pool = new Pool({
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_DATABASE || 'kinoX',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '2442',
-};
-
-const pool = new Pool(dbConfig);
-
-pool.on('connect', () => {
-  console.log('PostgreSQL pool connected!');
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false // важно для Render
+  }
 });
-
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
-
-// Добавляем метод getClient для работы с транзакциями
-pool.getClient = async () => {
-  return await pool.connect();
-};
 
 module.exports = pool;
